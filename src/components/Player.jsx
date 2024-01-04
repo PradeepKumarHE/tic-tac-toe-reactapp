06eec4d1a4ae07d9de54cafc62bf9c0a231e358b
Pradeep <pradeepkumarhe1989@gmail.com>
2024-01-04 20:00:49 +0530

    displaying textbox on click of button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+  let playerName = <span className="palyer-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
   return (
     <li>
       <span className="palyer">
-        <span className="palyer-name">{name}</span>
+        {playerName}
         <span className="palyer-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Save</button>
     </li>
   );
 }
