b1df0884aa75ccd42bcb315bf34ab5113064a6f6
Pradeep <pradeepkumarhe1989@gmail.com>
2024-01-05 22:20:10 +0530

    GameBoard Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Pradeep" symbol="X" />
           <Player initialName="Pramod" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
